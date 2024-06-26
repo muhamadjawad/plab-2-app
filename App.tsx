68a5bc8f9fd4fabb1e61657c587b542aef5edaae
muhamadjawad <jawadmansoor1998@gmail.com>
2024-06-26 16:52:41 +0500

    absolutepath implemented

--- App.tsx
+++ App.tsx
@@ -1,19 +1,19 @@
 import React, {Component, useEffect} from 'react';
 import {View} from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
-import colors from './src/styles/colors';
-import Home from './src/screens/Home';
+import QuestionReading from '@src/screens/QuestionReading';
+import colors from '@src/styles/colors';
 
 const App = () => {
   useEffect(() => {
     setTimeout(() => {
       SplashScreen.hide();
-    }, 2000);
+    }, 1000);
   }, []);
 
   return (
     <View style={{flex: 1, backgroundColor: colors.white}}>
-      <Home />
+      <QuestionReading />
     </View>
   );
 };
---
+++ src/screens/QuestionReading/index.tsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import { StyleSheet, View } from 'react-native';
+// import AppHeader from '../../components/AppHeader';
+import AppHeader from '@src/components/AppHeader';
+
+const QuestionReading = () => {
+    return (
+        <View>
+            <AppHeader title="Read Question Time" />
+        </View>
+        );
+}
+
+const styles = StyleSheet.create({
+    main: {
+        flex: 1
+    }
+})
+export default QuestionReading;
